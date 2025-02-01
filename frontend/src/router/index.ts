537d95fbf74a0bb7f9afd0474d6f9dd5fc5d7e49
Matheus Kagohara <matheuskagohara@gmail.com>
2025-02-01 06:29:05 -0300

    Refactored many things, some of most importants are the auth to make an request, and the axios configuration also havethe theme configuration and the adjustments in layout to use the nav bar and the sidebar

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,30 +1,49 @@
 //src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import StudentsView from '@/views/StudentsView.vue'
+import LoginView from '@/views/LoginView.vue'
+import { useAuthStore } from '@/stores/auth'
+import StudentsFormView from '@/views/StudentsFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/login', component: LoginView, meta: { noSidebar: true } },
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: StudentsView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/studentList',
       name: 'Student',
       component: StudentsView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/studentForm',
+      name: 'StudentForm',
+      component: StudentsFormView,
+      meta: { requiresAuth: true },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (!authStore.token) {
+    const storedToken = localStorage.getItem('token')
+    if (storedToken) {
+      authStore.setToken(storedToken)
+    }
+  }
+
+  if (to.meta.requiresAuth && !authStore.token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
