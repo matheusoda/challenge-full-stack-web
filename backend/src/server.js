40d3b914f26349577898ed8e4e42e20aa8258c5e
Matheus Kagohara <matheuskagohara@gmail.com>
2025-02-01 06:34:12 -0300

    Added the swagger at the project and maked the configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/routes.js";
 import cors from "cors";
+import swaggerJSDoc from"swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,38 @@ const PORT = process.env.PORT || 3000;
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Exemplo",
+            version: "1.0.0",
+            description: "Uma API de exemplo para integrar o Swagger"
+        },
+        servers: [
+            {
+                url: `http://localhost:${PORT}/api`
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT"
+                }
+            }
+        },
+        security: [{ bearerAuth: [] }]
+    },
+    apis: ["./src/routes/routes.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
